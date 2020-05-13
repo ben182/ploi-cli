6859ab39ec37e57e942b004fd866ca9067cf86ff
Ben <moin@benjaminbortels.de>
2020-05-13 22:36:22 +0200

    wip

--- app/Helper/Bash.php
+++ app/Helper/Bash.php
@@ -6,7 +6,7 @@ class Bash
 {
     public function command_exist($cmd)
     {
-        $return = shell_exec(sprintf("which %s 2>/dev/null", escapeshellarg($cmd)));
+        $return = shell_exec(sprintf("type -t %s", escapeshellarg($cmd)));
 
         return ! empty($return);
     }
